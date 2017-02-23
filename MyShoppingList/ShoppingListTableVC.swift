cd9a3ced4beaf61c4bb60745b2dd51c414a979d5
daxapps <daxapps777@gmail.com>
2017-02-23 10:09:06 -0600

    Create Custom View to Add New Shopping List
    - integrate with Core Data

---
+++ MyShoppingList/ShoppingListTableVC.swift
@@ -0,0 +1,141 @@
+//
+//  ShoppingListTableVC.swift
+//  MyShoppingList
+//
+//  Created by Jason Crawford on 2/23/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ShoppingListTableVC: UITableViewController {
+    
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initializeCoreDataStack()
+    }
+
+    func initializeCoreDataStack() {
+        
+        guard let modelURL = Bundle.main.url(forResource: "MyGroceryDataModel", withExtension: "momd") else {
+            fatalError("GroceryDataModel not found")
+        }
+        
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to initialize ManagedObjectModel")
+        }
+        
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        
+        let fileManager = FileManager()
+        
+        guard let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError("Unable to get documents URL")
+        }
+        
+        let storeURL = documentsURL.appendingPathComponent("MyGrocery.sqlite")
+        
+        print(storeURL)
+        
+        try! persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+        
+        let type = NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType
+        self.managedObjectContext = NSManagedObjectContext(concurrencyType: type)
+        self.managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: 44))
+        headerView.backgroundColor = UIColor.lightText
+        
+        let textField = UITextField(frame: headerView.frame)
+        textField.placeholder = "Enter Shopping List"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height:0))
+        textField.leftViewMode = .always
+        
+        headerView.addSubview(textField)
+        
+        return headerView
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
